698243f5fd07374982a465d26a684afce9f05374
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-03-22 18:37:35 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        String strTrim = "  Java  ";
+        System.out.println(strTrim);
+        String actualValue = strTrim.trim();
+        System.out.println(actualValue);
+
+    }
+}
